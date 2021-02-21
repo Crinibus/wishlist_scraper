ad8cc18388e36fbfdbc92429e0883568a4fd0db2
Crinibus <57172157+Crinibus@users.noreply.github.com>
2021-02-21 21:34:40 +0100

    Update program.py
    Delete unused functions

--- program.py
+++ program.py
@@ -3,43 +3,6 @@ import questionary
 import data as dt
 
 
-def read_wishlist(filename: str) -> dict:
-    with open(filename, "r") as json_file:
-        data = json.load(json_file)
-    return data
-
-
-def get_super_categories(data: dict) -> list:
-    temp = []
-    for super_cat in data.keys():
-        temp.append(super_cat)
-    return temp
-
-
-def get_sub_categories(data: dict, super_category: str) -> list:
-    temp = []
-    for sub_cat in data[super_category]:
-        temp.append(sub_cat)
-    return temp
-
-
-def get_links(data: dict, super_category: str, sub_category: str) -> list:
-    temp = []
-    for link in data[super_category][sub_category]:
-        temp.append(link)
-    return temp
-
-
-# if __name__ == '__main__':
-#     data = read_wishlist('wishlist.json')
-#     super_cat = questionary.select("Select a category:", choices=get_super_categories(data)).ask()
-#     sub_cat = questionary.select("Select a sub category:", choices=get_sub_categories(data, super_cat)).ask()
-
-#     print()
-#     for index, link in enumerate(get_links(data, super_cat, sub_cat)):
-#         print(f"{index + 1} > {link}")
-
-
 def show_all_data(data: dt.Data):
     for superCategory in data.categories:
         print(f"\nSuper category: {superCategory.name}")
